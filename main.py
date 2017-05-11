25aeec54df1d2c26b85ee9d8c8b36f320bce9274
kgunnit <kgunnit@gmail.com>
2017-05-11 10:00:42 -0600

    Additional comments in code

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ import tkinter
 
 
 def main():
+#   creates the window container
     base = tkinter.Tk()
+# creates the label, pack actually adds the label to the window
     label1 = tkinter.Label(base, text="Hello, this is a window.")
     label1.pack()
+# mainloop keeps the window displayed until closed
     base.mainloop()
     
 if __name__ == '__main__':
