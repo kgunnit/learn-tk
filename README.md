f02011f34788be7a1071cd77c29651a0fbca3ed8
kgunnit <kgunnit@gmail.com>
2017-05-14 23:42:43 -0600

    Tutorial 3 - sizing and layout

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # learn-tk
 Learning to create a GUI in Python using Tkinter
+
+Source:     https://www.youtube.com/playlist?list=PL6gx4Cwl9DGBwibXFtPtflztSNPGuIB_d
--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 # working on getting familiar with creating gui in python
 
-import tkinter
-
+from tkinter import *
 
 def main():
 #   creates the window container
-    base = tkinter.Tk()
-# creates the label, pack actually adds the label to the window
-    label1 = tkinter.Label(base, text="Hello, this is a window.")
-    label1.pack()
+    base = Tk()
+#   Add labels to different regions in a window
+    topLabel = Label(base, text="Top", bg="Orange", fg="blue")
+    topLabel.pack()
+
+    middleLabel = Label(base, text="Second Label - Fill X", bg="black", fg="Orange")
+    middleLabel.pack(fill=X)
+
+    bottomLabel = Label(base, text="Third Label - Fill Y", bg="red", fg="white")
+    bottomLabel.pack(side=LEFT, fill=Y)
+
+    lastLabel = Label(base, text="Last Label - Fill Both", bg="yellow", fg="black")
+    lastLabel.pack(fill=BOTH, expand=True)
+
 # mainloop keeps the window displayed until closed
     base.mainloop()
     
